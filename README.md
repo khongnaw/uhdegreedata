192fdb54bfdd988e261d1dd7fcc53f8e70a6ce60
sora <android.prok@gmail.com>
2015-09-29 22:32:02 -1000

    Standard compliant analytic functions for UH dataset

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# uhdegreedata
-Analytics for UH degrees
+# Overview
+
+Provides seven functions computing analytics over the [Hawaii Open Dataset
+for UH Degree Data](https://data.hawaii.gov/Formal-Education/University-Of-Hawaii-Degrees-Awarded-By-Major-CIP-/7bfs-svqv).
+
+# Installation
+
+Provide the following scripts in your html file.
+
+```
+<script src="//philipmjohnson.github.io/ics314f15/morea/underscore/underscore-min.js"></script>
+<script src="//philipmjohnson.github.io/ics314f15/morea/underscore/uhdata.js"></script>
+<script src="uhdegreedata.js"></script>
+
+```
+
+# Usage
+
+Here are example calls to the analytic functions:
+
+```
+<script>
+  console.log("Total Degrees", totalDegrees(uhdata));
+  console.log("Percentage Hawaiian", percentageHawaiian(uhdata));
+  console.log("Total Degrees By Year", totalDegreesByYear(uhdata, 2012));
+  console.log("List Campuses", listCampuses(uhdata));
+  console.log("List Campus Degrees", listCampusDegrees(uhdata));
+  console.log("Max Degrees", maxDegrees(uhdata));
+  console.log("Doctoral Degree Programs", doctoralDegreePrograms(uhdata));
+</script>
+
+```
+
+Consult the uhdegreedata.js file for more details on these functions.
+
+# Credits
+
+Uses the [Underscore](http://underscorejs.org/).
--- uhdegreedata.js
+++ uhdegreedata.js
@@ -1,56 +1,74 @@
-/**
+/*
+ * Analytics for UH Data sets
  * Created by sora1234 on 9/26/2015.
  */
 
-var test = uhdata.splice(0,3);
+/* globals _, uhdata*/
+/* exported testdata, percentageHawaiian, totalDegreesByYear, listCampuses*/
+/* exported listCampusDegrees, maxDegrees, doctoralDegreePrograms */
 
 /**
- * totalDegrees, this function returns the total number of degrees awarded in the uhdata set
- * @param data, uhdata set.
+ * Provides a small set of records from the UH dataset
+ * @type {string}
  */
-function totalDegrees(data){
-  return _.reduce(data,function(memo,num){
-    return memo + num["AWARDS"];
-  },0);
-};
+var testdata = uhdata.slice(0,2);
 
 /**
- * percentageHawaiian, this function returns the percentage of degrees that were awarded to students of Hawaiian Legacy in the uhdata set.
- * @param data, uhdata set
+ * This function returns the total number of degrees awarded in the uhdata set
+ * @param data, The UH dataset.
+ * @returns The total number of degrees
+ */
+function totalDegrees(data) {
+  return _.reduce(data, function (memo, value) {
+    return memo + value["AWARDS"];
+  }, 0);
+}
+
+/**
+ * This function returns the percentage of degrees awarded to those of
+ * Hawaiian ancestry
+ * @param datam The UH dataset
+ * @returns Percentage degrees to the Hawaiians
  */
 
-function percentageHawaiian(data){
-  var totalHawaiianAward = _.reduce(data,function(memo,num){
-    if(num["HAWAIIAN_LEGACY"] === "HAWAIIAN")
+function percentageHawaiian(data) {
+  var totalHawaiianAward = _.reduce(data, function (memo, num) {
+    if (num["HAWAIIAN_LEGACY"] === "HAWAIIAN") {
       return memo + num["AWARDS"];
-    else
+    }
+    else {
       return memo + 0;
-  },0)
+    }
+  }, 0);
 
-  return 100*(totalHawaiianAward/totalDegrees(data));
-};
+  return 100 * (totalHawaiianAward / totalDegrees(data));
+}
 
 /**
- * totalDegreesByYear, this function returns a number of degrees awarded in the passed year.
- * @param data, uhdata set
- * @param year
+ * This function returns a number of degrees awarded in the a given year.
+ * @param data, The UH dataset
+ * @param year, The year of interest
+ * @returns The total degrees for that year.
  */
 
-function totalDegreesByYear(data,year){
-  return _.reduce(data,function(memo,num){
-    if(num["FISCAL_YEAR"] === year)
-      return memo +  num["AWARDS"];
-    else
+function totalDegreesByYear(data, year) {
+  return _.reduce(data, function (memo, num) {
+    if (num["FISCAL_YEAR"] === year) {
+      return memo + num["AWARDS"];
+    }
+    else {
       return memo + 0;
-  },0)
-};
+    }
+  }, 0);
+}
 
 /**
- * listCampuses, this function returns an array containing all the campuses referenced in the passed dataset.
- * @param data, uhdata set
+ * This function returns the campuses in the passed dataset
+ * @param data, The UH dataset
+ * @returns An array of strings, one for each campus in the dataset
  */
-function listCampuses(data){
-  var campusNames = _.pluck(data,'CAMPUS');
+function listCampuses(data) {
+  var campusNames = _.pluck(data, 'CAMPUS');
   return _.uniq(campusNames);
 }
 
@@ -59,46 +77,49 @@ function listCampuses(data){
  * @param data, uhdata set
  */
 
-function listCampusDegrees(data){
-  var groupByCampus = _.groupBy(data,function(num){
+function listCampusDegrees(data) {
+  var groupByCampus = _.groupBy(data, function (num) {
     return num["CAMPUS"];
   });
-  return _.mapObject(groupByCampus,function(val){
-    return _.reduce(val,function(memo,num){
+  return _.mapObject(groupByCampus, function (val) {
+    return _.reduce(val, function (memo, num) {
       return memo + num["AWARDS"];
-    },0);
+    }, 0);
   });
-};
+}
 
 /**
- * maxDegrees, this function returns an integer indicating the maximum number of degrees awarded in a year.
- * @param data, uhdata set
+ * This function returns the maximum number of degrees awarded in a single
+ * year in the dataset
+ * @param data, The UH dataset
+ * @returns The maximum number of degrees.
  */
 
-function maxDegrees(data){
-  var groupByYear = _.groupBy(data,function(val){
+function maxDegrees(data) {
+  var groupByYear = _.groupBy(data, function (val) {
     return val["FISCAL_YEAR"];
   });
 
-  var mapAwards = _.map(groupByYear,function(val){
-    return _.reduce(val,function(memo,num){
+  var mapAwards = _.map(groupByYear, function (val) {
+    return _.reduce(val, function (memo, num) {
       return memo + num["AWARDS"];
-    },0)
+    }, 0);
   });
   return _.max(mapAwards);
 }
 
 /**
- * doctoralDegreePrograms, this function returns a list of the degree programs ("CIP_DESC") for which a doctoral degree is granted
- * @param data, uhdata set
+ * This function returns a list of programs with a doctoral degree.
+ * @param data, The UH dataset
+ * @returns A list of string, one per program with a doctoral degree
  */
 
-function doctoralDegreePrograms(data){
-  var listDoc = _.filter(data,function(val){
+function doctoralDegreePrograms(data) {
+  var listDoc = _.filter(data, function (val) {
     return val["OUTCOME"] === "Doctoral Degrees";
   });
 
-  var pluckDeg = _.pluck(listDoc,'CIP_DESC');
+  var pluckDeg = _.pluck(listDoc, 'CIP_DESC');
 
   return _.uniq(pluckDeg);
 
